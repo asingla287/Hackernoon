6406d9cde3e5a9a7cf2d1202daa45dac7d977b63
amansin <amansin@raet.com>
2018-11-27 16:12:40 +0530

    added mutations including optional

--- server/src/index.js
+++ server/src/index.js
@@ -1,43 +1,46 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
-// 2
-// 1
 let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-  
-  const resolvers = {
-    Query: {
-      info: () => `This is the API of a Hackernews Clone`,
-      // 2
-      feed: () => links,
-    },
-    // 3
-    Link: {
-      id: (root) => root.id,
-      description: (root) => root.description,
-      url: (root) => root.url,
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (root,args) => {
+      return links.find(x => x.id == args.id)
     }
+  },
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link)
+      return link
+    },
+    deleteLink: (root,args)=>{
+      let index = links.findIndex(x=> x.id == args.id)
+      let deletedLink  = links.splice(index,1)
+      return deletedLink[0]
+    },
+    updateLink: (root,args)=>{
+      let index = links.findIndex(x=> x.id == args.id)
+      links[index].description = args.description
+      links[index].url = args.url
+      return links[index]
+    },
   }
-// 3
+}
+
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
