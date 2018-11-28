5c1bd0678711bd50893401b5b13f168f631599b5
amansin <amansin@raet.com>
2018-11-28 18:11:14 +0530

    added pagination

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,12 +1,18 @@
 function feed(parent, args, context, info) {
-  const where = args.filter ? {
-    OR: [
-      {url_contains: args.filter},
-      {description_contains: args.filter},
-    ]
-  } : {}
-    return context.db.query.links({ where }, info)
-  }
+  const where = args.filter
+    ? {
+        OR: [
+          { url_contains: args.filter },
+          { description_contains: args.filter },
+        ],
+      }
+    : {}
+
+  return context.db.query.links(
+    { where, skip: args.skip, first: args.first },
+    info,
+  )
+}
   
   module.exports = {
     feed,
